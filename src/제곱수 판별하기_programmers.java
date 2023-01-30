68e3394802de70840bbe0df60bf566934963c289
Jiyoung Kim <105136541+dancing-potato@users.noreply.github.com>
2023-01-30 23:17:22 +0900

    제곱수 판별하기

---
+++ src/제곱수 판별하기_programmers.java
@@ -0,0 +1,13 @@
+
+
+class Solution {
+    public int solution(int n) {
+        if (n % Math.sqrt(n) == 0) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
+
+
